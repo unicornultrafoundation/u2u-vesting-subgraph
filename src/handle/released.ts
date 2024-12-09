d17d87ee756e1d61c3612f6564f03a4716fff0fe
ngdlong91 <ngdlong91@gmail.com>
2024-12-09 10:32:31 +0700

    Update event handler

--- src/handle/released.ts
+++ src/handle/released.ts
@@ -1,6 +1,41 @@
 import {Released} from "../../generated/templates/VestingPool/VestingPool";
+import {log} from "@graphprotocol/graph-ts";
+import {ReleaseRecord, UserInfo, VestingPool} from "../../generated/schema";
+import {ONE_BI} from "../helper";
 
 
 export function released(e: Released): void {
-  // Do nothing now
+  log.info("New release with txHash: {}, block: {}", [e.transaction.hash.toHexString(), e.block.number.toString()])
+
+  let vestingPoolID = e.address.toHexString();
+  let vestingPool = VestingPool.load(vestingPoolID);
+  if (!vestingPool) {
+    log.info("cannot withdraw from invalid vesting pool {}", [vestingPoolID]);
+    return;
+  }
+
+  let userId = e.params.beneficiary.toHexString();
+  let userInfo = UserInfo.load(userId);
+  if (!userInfo) {
+    log.info("cannot withdraw from invalid user {}", [vestingPoolID]);
+    return;
+  }
+  userInfo.releasedAmount = userInfo.releasedAmount.plus(e.params.releasedAmount);
+  userInfo.completedPeriods = userInfo.completedPeriods.plus(ONE_BI);
+
+  let releaseID = e.transaction.hash.toHexString();
+  let releaseRecord = ReleaseRecord.load(releaseID);
+  if (!releaseRecord) {
+    releaseRecord = new ReleaseRecord(releaseID);
+    releaseRecord.hash = e.transaction.hash;
+    releaseRecord.vestingPool = vestingPool.id;
+    releaseRecord.user = userInfo.id;
+    releaseRecord.amount = e.params.releasedAmount;
+    releaseRecord.releasedPeriods = e.params.releasedPeriods;
+    releaseRecord.releaseTime = e.block.timestamp;
+  }
+
+  vestingPool.save();
+  userInfo.save();
+  releaseRecord.save();
 }
--- src/handle/withdraw.ts
+++ src/handle/withdraw.ts
@@ -1,6 +1,37 @@
 import {Withdrawn} from "../../generated/VestingFactory/VestingFactory";
+import {log} from "@graphprotocol/graph-ts";
+import {UserInfo, VestingPool, WithdrawnRequest} from "../../generated/schema";
 
 
 export function withdraw(e: Withdrawn): void {
+  log.info("New withdraw with txHash: {}, block: {}", [e.transaction.hash.toHexString(), e.block.number.toString()])
 
+  let vestingPoolID = e.params.poolAddr.toHexString();
+  let vestingPool = VestingPool.load(vestingPoolID);
+  if (!vestingPool) {
+    log.info("cannot withdraw from invalid vesting pool {}", [vestingPoolID]);
+    return;
+  }
+
+  let userId = e.params.beneficiary.toHexString();
+  let userInfo = UserInfo.load(userId);
+  if (!userInfo) {
+    log.info("cannot withdraw from invalid user {}", [vestingPoolID]);
+    return;
+  }
+
+  let withdrawID = e.transaction.hash.toHexString();
+  let withdrawRequest = WithdrawnRequest.load(withdrawID);
+  if (!withdrawRequest) {
+    withdrawRequest = new WithdrawnRequest(withdrawID);
+    withdrawRequest.hash = e.transaction.hash;
+    withdrawRequest.vestingPool = vestingPool.id;
+    withdrawRequest.user = userInfo.id;
+    withdrawRequest.amount = e.params.amount;
+    withdrawRequest.withdrawTime = e.block.timestamp;
+  }
+
+  vestingPool.save()
+  userInfo.save()
+  withdrawRequest.save();
 }
