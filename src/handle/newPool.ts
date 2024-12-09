3d865aa2cb40b434b24fcb4cb622fc8f1bccd7c1
ngdlong91 <ngdlong91@gmail.com>
2024-12-09 09:37:58 +0700

    Update new pool

--- src/handle/newPool.ts
+++ src/handle/newPool.ts
@@ -1,13 +1,22 @@
 import { log, BigInt } from "@graphprotocol/graph-ts"
 import {NewPool} from "../../generated/VestingFactory/VestingFactory";
 import {VestingPool as VestingPoolTemplate} from "../../generated/templates";
+import {VestingPool} from "../../generated/schema";
 
 /**
  * Claimed rewards event handle
  * @param event
  */
 export function newPool(e: NewPool): void {
-  log.info("Create new pool with txHash: {}, block: {}", [e.transaction.hash.toHexString(), e.block.number.toString()])
-  VestingPoolTemplate.create(e.params.id)
+  log.info("New pool event with txHash: {}, block: {}", [e.transaction.hash.toHexString(), e.block.number.toString()])
 
+  let vestingPoolID = e.params.id.toHexString();
+  let vestingPool = VestingPool.load(vestingPoolID);
+  if (!vestingPool) {
+    log.info("Create new pool with contract: {}", [vestingPoolID])
+    vestingPool = new VestingPool(vestingPoolID);
+    vestingPool.save();
+  }
+
+  VestingPoolTemplate.create(e.params.id)
 }
